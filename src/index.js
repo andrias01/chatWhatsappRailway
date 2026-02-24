f2df708fc517500ac35387c46d0648d6faacc8fd
Andres Velez <andres.velez5136@uco.net.co>
2026-02-24 00:19:03 -0500

    test de verificaicion3

--- src/index.js
+++ src/index.js
@@ -6,14 +6,8 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
+app.use("/api", apiruta);
 
-app.use("/api",apiruta);
-
-/* app.listen(PORT,() => {
-    console.log("Hola AnderCode v1 el puerto es :" + PORT);
-}) */
-
-//TODO: Configura el servidor para escuchar en todas las interfaces (0.0.0.0)
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log("Hola Andres v1 el puerto es :" + PORT);
 });
